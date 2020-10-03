8ff25c1bcad95f2939da00dcdefa9bf41650b95e
whiscovery <whiscovery@gmail.com>
2020-10-04 02:13:14 +0900

    firebase

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyDLZtX60qH_b0IoWMfPqeJ-odR5IteBlfc",
+  authDomain: "whiscovery-3f35a.firebaseapp.com",
+  databaseURL: "https://whiscovery-3f35a.firebaseio.com",
+  projectId: "whiscovery-3f35a",
+  storageBucket: "whiscovery-3f35a.appspot.com",
+  messagingSenderId: "233157045701",
+  appId: "1:233157045701:web:f93aefa2462a5343d9afac"
+}
